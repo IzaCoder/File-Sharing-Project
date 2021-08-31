be6b16725aaf0807d58371b9ec8519fa536e840d
IzaCoder <85595031+IzaCoder@users.noreply.github.com>
2021-08-31 01:47:49 -0400

    Update data_handling.py
    Add docstring (part 1)

--- helpers/data_handling.py
+++ helpers/data_handling.py
@@ -4,5 +4,20 @@ import json
 BASE_PATH = "./data/"
 
 def get(data):
+    """
+    This method returns data from the specified JSON file (don't include the extension, nor path).
+    If data == users,
+    this method returns a dictionary containing user info for all users. The dictionary is formatted like (types are in parentheses):
+    {
+        username (string): {
+            "username": username (string),
+            "documents": documents (list),
+            "groups": groups (list),
+            "folders": folders (list)
+        }
+    }
+    To access this dictionary (assuming the return value of this method is assigned to `users`), use `users[username]`.
+    Then, to access a specific property of that user, use `users[username][property]`.
+    """
     with open(BASE_PATH + data + ".json", "r") as data_file:
         return json.loads(data_file.read())
