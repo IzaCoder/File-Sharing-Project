eacc386f5ce428f947af9d768a35732f65b74e6b
IzaCoder <85595031+IzaCoder@users.noreply.github.com>
2021-08-31 02:00:40 -0400

    Add comments
    Add comments throughout file

--- helpers/data_handling.py
+++ helpers/data_handling.py
@@ -3,6 +3,7 @@ import json
 # Declare constant
 BASE_PATH = "./data/"
 
+# Define method to get data from file
 def get(data):
     """
     This method returns data from the specified JSON file (don't include the extension, nor path).
@@ -19,10 +20,15 @@ def get(data):
     To access this dictionary (assuming the return value of this method is assigned to `users`), use `users[username]`.
     Then, to access a specific property of that user, use `users[username][property]`.
     """
+    
+    # Open data file
     with open(BASE_PATH + data + ".json", "r") as data_file:
+        # Return a dictionary from the JSON
         return json.loads(data_file.read())
 
- 
+# Define method to update the data in a file
 def update(data_file, data):
+    # Open data file in write mode
     with open(BASE_PATH + data_file + ".json", "w") as data_file:
+        # Write JSON string to file
         data_file.write(json.dumps(data))
