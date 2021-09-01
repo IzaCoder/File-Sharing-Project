a40be5e1ffa713dc753725f4e9ba252730d5a4ad
IzaCoder <85595031+IzaCoder@users.noreply.github.com>
2021-08-31 21:40:35 -0400

    Remove (hopefully) unused functions

--- helpers/users.py
+++ helpers/users.py
@@ -6,29 +6,6 @@ import helpers.data_handling
 # Declare variables
 USERS_PATH = "./data/users.json"
 
-def get():
-    """
-    This method returns a dictionary containing user info for all users. The dictionary is formatted like (types are in parentheses):
-    {
-        username (string): {
-            "username": username (string),
-            "documents": documents (list),
-            "groups": groups (list),
-            "folders": folders (list)
-        }
-    }
-    To access this dictionary (assuming the return value of this method is assigned to `users`), use `users[username]`.
-    Then, to access a specific property of that user, use `users[username][property]`.
-    """
-
-    # Open data file
-    with open(USERS_PATH, "r") as f:
-        # Return dictionary described in docstring
-        return json.loads(f.read())
-
-def update(users):
-    with open(USERS_PATH, "w") as f:
-        f.write(json.dumps(users))
 
 def new_user(username, password):
     # Get variables
